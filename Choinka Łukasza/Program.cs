ddf4657d2d27d02882449b4685163325f6dda8d7
Patryk Padoł <40404592+Sboov@users.noreply.github.com>
2024-12-25 08:50:28 +0100

    1.0

---
+++ Choinka Łukasza/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Choinka_Łukasza
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 4;
+            for (int poziom = 0; poziom < n; poziom++)
+            {
+                int szerokosc = 1 + 2 * poziom; 
+                int spacje = n - poziom - 1; 
+
+
+                for (int i = 0; i < spacje; i++)
+                {
+                    Console.Write(" ");
+                }
+
+
+                for (int i = 0; i < szerokosc; i++)
+                {
+                    Console.Write("#");
+                }
+
+                Console.WriteLine();
+            }
+            for (int i = 0; i < n - 1; i++)
+            {
+                Console.Write(" ");
+            }
+            Console.WriteLine("#");
+        }
+    }
+}
